0b6c4c9fa870ee2bd06b397897bcb3c905d09f0f
Jeffrey-Ho-OneAi <jeffrey.ho@one-ai.inc>
2024-11-14 14:32:17 +0800

    looks like can build

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+import VFileSystemContainer from './components/VFileSystemContainer.vue'
+import VFileSystemItem from './components/VFileSystemItem.vue'
+
+export { VFileSystemContainer, VFileSystemItem }
+
+export { useVFsGhostSelector } from './composables/useVFsGhostSelector'
+export { useVFsItemClickChain } from './composables/useVFsItemClickChain'
+export { useVFsItemDragChain } from './composables/useVFsItemDragChain'
+export { useVFsSelection } from './composables/useVFsSelection'
