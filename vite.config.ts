60c5bb0206242b51d8815f446886522c75ca7f90
Jeffrey-Ho-OneAi <jeffrey.ho@one-ai.inc>
2024-12-05 10:40:13 +0800

    0.0.61

--- vite.config.ts
+++ vite.config.ts
@@ -12,7 +12,7 @@ export default defineConfig({
   build: {
     lib: {
       entry: resolve(__dirname, 'src/index.ts'),
-      name: 'VFileSystem',
+      name: 'VFileGrid',
       fileName: format => `v-file-grid.${format}.js`,
     },
     rollupOptions: {
