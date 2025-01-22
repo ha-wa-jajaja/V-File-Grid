f1aac9e25fbdf7aa2c528a417c56eeb7d7758342
Jeffrey-Ho-OneAi <jeffrey.ho@one-ai.inc>
2025-01-22 10:16:54 +0800

    Complete refactor vfg selection

--- src/composables/useVFgSelection.ts
+++ src/composables/useVFgSelection.ts
@@ -5,33 +5,6 @@ export const useVFgSelection = (
   selectedIdModel: SelectedIdsModel,
   allIds: Ref<Array<string | number>>,
 ) => {
-  function handleMultiSelection(
-    selectedIds: Set<string | number>,
-    targetId: string | number,
-  ) {
-    const selectedItemIndexes = Array.from(selectedIds).map(selectedId =>
-      allIds.value.findIndex(i => i === selectedId),
-    )
-    const currItemIdx = allIds.value.findIndex(i => i === targetId)
-    const firstItemIdx = Math.min(...selectedItemIndexes)
-    const lastItemIdx = Math.max(...selectedItemIndexes)
-
-    if ([firstItemIdx, lastItemIdx, currItemIdx].some(n => n < 0))
-      throw new Error('Id not found in id list')
-
-    let itemsToAdd
-
-    if (currItemIdx < firstItemIdx) {
-      itemsToAdd = allIds.value.slice(currItemIdx, lastItemIdx)
-    } else {
-      const startIdx = currItemIdx < lastItemIdx ? currItemIdx : lastItemIdx + 1
-      const endIdx = currItemIdx < lastItemIdx ? lastItemIdx : currItemIdx + 1
-
-      itemsToAdd = allIds.value.slice(startIdx, endIdx)
-    }
-    itemsToAdd.forEach(i => selectedIdModel.value?.add(i))
-  }
-
   function updateSelectedIdModel(
     action: 'clear' | 'select' | 'delete' | 'append' | 'add-multi',
     id?: string,
@@ -63,6 +36,40 @@ export const useVFgSelection = (
     if (action === 'delete') {
       if (selectedIdModel.value.has(id)) selectedIdModel.value.delete(id)
     }
+  }
+
+  function handleMultiSelection(
+    selectedIds: Set<string | number>,
+    targetId: string | number,
+  ) {
+    const selectedItemIndexes: number[] = []
+    let targetIdIdx = -1
+    let firstItemIdx = Infinity
+    let lastItemIdx = -1
+
+    allIds.value.forEach((id, idx) => {
+      if (selectedIds.has(id)) {
+        selectedItemIndexes.push(idx)
+        firstItemIdx = Math.min(firstItemIdx, idx)
+        lastItemIdx = Math.max(lastItemIdx, idx)
+      }
+      if (id === targetId) targetIdIdx = idx
+    })
+
+    if ([firstItemIdx, lastItemIdx, targetIdIdx].some(n => n < 0))
+      throw new Error('Selected list has mismatch with allIds list')
+
+    let itemsToAdd
+    if (targetIdIdx < firstItemIdx) {
+      itemsToAdd = allIds.value.slice(targetIdIdx, lastItemIdx)
+    } else {
+      const startIdx = targetIdIdx < lastItemIdx ? targetIdIdx : lastItemIdx + 1
+      const endIdx = targetIdIdx < lastItemIdx ? lastItemIdx : targetIdIdx + 1
+
+      itemsToAdd = allIds.value.slice(startIdx, endIdx)
+    }
+
+    itemsToAdd.forEach(i => selectedIdModel.value?.add(i))
   }
 
   return { updateSelectedIdModel }
